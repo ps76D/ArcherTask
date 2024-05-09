7a0c33164ac0cfcb00da29a16672be9e79861313
Denis Gustov <DGustov@synergy.ru>
2024-05-09 19:23:10 +0300

    большой рефакторинг и красивости

---
+++ trunk/Project/Assets/TaskArcher/Scripts/Units/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace TaskArcher.Units
+{
+    public class Enemy : Unit
+    {
+        public Action<Animator, string> OnChangeAnim;
+        public Action<Unit, float> OnTakeDamage;
+        
+        private void OnEnable()
+        {
+            OnChangeAnim += SetAnim;
+            OnTakeDamage += TakeDamage;
+        }
+
+        private void OnDisable()
+        {
+            OnChangeAnim -= SetAnim;
+            OnTakeDamage -= TakeDamage;
+        }
+    }
+}
